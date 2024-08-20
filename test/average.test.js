c26a420c9e416362713d0839816514a818eed1f4
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-08-20 10:31:20 -0400

    FSO_Blog_modifide_test

--- test/average.test.js
+++ test/average.test.js
@@ -1,14 +1,18 @@
-const {test} = require('node:test');
+const {test, describe} = require('node:test');
 const assert = require('node:assert');
 
 const average = require('../utils/for_testing').average;
 
-test('average of one value is the value itself', () => {
-  assert.strictEqual(average([7]), 7);
-});
-test('average of many is calculated correctly', () => {
-  assert.strictEqual(average([1, 2, 3]), 2);
-});
-test('average of empty array is zero', () => {
-  assert.strictEqual(average([]), 0);
+describe('average', () => {
+  test('average of one value is the value itself', () => {
+    assert.strictEqual(average([7]), 7);
+  });
+
+  test('average of many is calculated correctly', () => {
+    assert.strictEqual(average([1, 2, 3]), 2);
+  });
+
+  test('average of empty array is zero', () => {
+    assert.strictEqual(average([]), 0);
+  });
 });
