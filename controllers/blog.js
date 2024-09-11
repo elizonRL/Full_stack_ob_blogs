8fa73d53b48e7365705bb15839ac2ab06a2db188
Elizon Rodrigues Liriano <118481749+elizonRL@users.noreply.github.com>
2024-09-11 10:50:05 -0400

    add blog user with jwt

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,15 +1,31 @@
+
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('/', async (req, res, next) => {
+
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   res.json(blogs)
 })
 
 blogRouter.post('/', async (req, res, next) => {
   const { title, author, url, likes } = req.body
-  const user = await User.findById('66e03ea59ecbc3e13d50fc3e')
+  const decodedToken = jwt.verify(getTokenFrom(req), config.SECRET)
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title,
     author,
